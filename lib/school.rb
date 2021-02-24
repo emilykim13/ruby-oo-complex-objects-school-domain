2b3bd5e2305e0fc737e8c6c1a2b106e667b63df3
Emily Nguyen <emilyn0614@gmail.com>
2021-02-23 18:44:19 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+require 'pry'
+
+class School
+    attr_accessor :roster, :name
+def initialize(name)
+    @name = name
+    @roster = {}
+end
+
+def add_student(student, grade)
+    if @roster[grade]
+        @roster[grade] << student
+    else
+        @roster[grade] = [student]
+    end
+end
+def grade(grade)
+    @roster[grade]
+end
+def sort
+    sort_students = {}
+    @roster.sort.each do |grade, names_arr|
+        sort_students[grade] = (names_arr.sort)
+    end
+    sort_students 
+end
+
+end
